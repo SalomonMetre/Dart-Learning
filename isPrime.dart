47c060ce6e3eef971cc18db5a43344c8a69384c3
Salomon Metre <64658915+SalomonMetre@users.noreply.github.com>
2021-08-04 22:43:56 +0300

    Test primality in Dart

---
+++ isPrime.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:math';
+
+isPrime(int n) {
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+main(List<String> args) {
+  print('Enter your number : ');
+  int n = int.parse(stdin.readLineSync().toString());
+  isPrime(n) ? print('$n is prime ! ') : print('$n is not prime !');
+}
