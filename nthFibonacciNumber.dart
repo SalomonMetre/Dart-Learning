031637bc7e30ae9e82257b4effbb8539bc8af911
Salomon Metre <64658915+SalomonMetre@users.noreply.github.com>
2021-08-04 23:24:42 +0300

    Find the nth Fibonacci number with Binet's formula

---
+++ nthFibonacciNumber.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:math';
+
+final phi = (1 + sqrt(5)) / 2;
+fibonacciNumber(int n) {
+  ///Let us find the nth Fibonacci number using Binet's formula
+  double result = (pow(phi, n) + pow(1 / phi, n)) / sqrt(5);
+  return result.toInt();
+}
+
+main(List<String> args) {
+  print('Enter the rank of the Fibonacci number you want : ');
+  int rank = int.parse(stdin.readLineSync().toString());
+  print('The ${rank}th fibonacciNumber(rank) is : ${fibonacciNumber(rank)}');
+}
