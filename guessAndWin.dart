db5472b5ee8015656ec330837c9339822f3a2b72
Salomon Metre <64658915+SalomonMetre@users.noreply.github.com>
2021-08-09 14:46:00 +0300

    Just a simple guessing game in Dart

--- guessAndWin.dart
+++ guessAndWin.dart
@@ -16,13 +16,14 @@ class GuessHelper {
   generateNumber() {
     print('Enter the maximum number that can be generated : ');
     String userChoice = stdin.readLineSync().toString();
-    generatedNumber = myGenerator?.nextInt(int.parse(userChoice)+1);
+    generatedNumber = myGenerator?.nextInt(int.parse(userChoice) + 1);
   }
 
   play() {
     do {
       print('Guess the number : ');
       userGuess = int.tryParse(stdin.readLineSync().toString());
+      print('Oh, so unlucky ! Please try again !');
       print('Number of trials : ${++nbTrials}');
     } while (userGuess != generatedNumber);
     print(' *** CONGRATULATIONS ***');
