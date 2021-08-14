ac73e3222b33e54dd3b5bf7b28a613f3b16fd502
Salomon Metre <64658915+SalomonMetre@users.noreply.github.com>
2021-08-14 18:09:40 +0300

    Who among your friends has the longest name?

---
+++ friendWithLongestName.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+main(List<String> args) {
+  bool canEnterFriendName = true;
+  List friendNames = <String>[];
+  while (canEnterFriendName) {
+    print('Enter the name of a friend : ');
+    friendNames.add(stdin.readLineSync().toString());
+    print('Do you want to enter another name?');
+    var userCanContinue = stdin.readLineSync().toString();
+    if (userCanContinue.toLowerCase() == 'no') canEnterFriendName = false;
+  }
+  friendNames.sort((a, b) => a.length.compareTo(b.length));
+  print('Your friends\' names from the longest the shortest : ');
+  print(friendNames);
+}
