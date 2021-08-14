65c1f6c3bfda0694d030e43f27dbc6ecf50ff8c3
Salomon Metre <64658915+SalomonMetre@users.noreply.github.com>
2021-08-15 00:34:36 +0300

    'Highest and lowest grades in a list'

---
+++ highestLowestGrade.dart
@@ -0,0 +1,13 @@
+///Highest grade and a few other tests on a list
+
+bool isGreaterThan50(int number) {
+  return number > 50;
+}
+
+main(List<String> args) {
+  var grades = [90, 20, 67, 54, 15, 72];
+  grades.sort();
+  print('Lowest grade : ${grades[0]}');
+  print('Highest grade : ${grades[grades.length - 1]}');
+  print('Grades greater than 50: ${grades.where(isGreaterThan50).toList()}');
+}
