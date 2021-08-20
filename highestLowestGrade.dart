9cc3032ce937e4cb261ea4f771fe3ef4d300b7ce
Salomon Metre <64658915+SalomonMetre@users.noreply.github.com>
2021-08-20 08:59:02 +0300

    A few manipulations on lists

--- highestLowestGrade.dart
+++ highestLowestGrade.dart
@@ -7,7 +7,7 @@ bool isGreaterThan50(int number) {
 main(List<String> args) {
   var grades = [90, 20, 67, 54, 15, 72, 30];
   grades.sort();
-  print('Lowest grade : ${grades[0]}');
+  print('Lowest grade : ${grades[0]} ');
   print('Highest grade : ${grades[grades.length - 1]}');
   print('Grades greater than 50: ${grades.where(isGreaterThan50).toList()}');
 }
